c790bf8c467b454be4b411ec95b71ef1ec15b889
Maycon Cabral <povei@outlook.com>
2021-06-24 14:33:07 -0300

    dog animation

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+const barkAudio = document.querySelector('audio')
+const btn = document.querySelector('button')
 
+//Blinking eyes 
+
+let blinking = gsap.timeline({repeatDelay:.3, repeat:-1, yoyo:true});
+blinking.to("#eyes", {delay:3, opacity:0, duration:.1}).to("#closedEyes", {opacity:1, duration:.2});
+
+//Dog's head movement
+let head = gsap.timeline({repeatDelay:1.5, repeat:-1, yoyo:true, duration:2.5});
+head.fromTo("#head", {rotation:-10, transformOrigin:"50% 50%", duration:1}, {rotation:10, transformOrigin:"50% 50%", duration:1.5});
+
+//Dog barking
+let mouth = gsap.timeline({delay:1, repeat:1, onComplete: quiet}).pause();
+mouth.to("#mouth",{y:12, duration:.8})
+function quiet(){
+    mouth.reverse(1)
+}
+btn.addEventListener('click', barking)
+
+function barking(){
+    mouth.play(0)
+    barkAudio.play()   
+}
