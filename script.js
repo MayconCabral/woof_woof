885edb946b19ec7690ef175ce70b8c5839d6e269
Maycon Cabral <povei@outlook.com>
2021-06-27 22:02:49 -0300

    Getting values from DataList

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const barkAudio = document.querySelector('audio')
-const btn = document.querySelector('button')
+const barkAudio = document.querySelector('audio');
+const btn = document.querySelector('button');
+const getValue = document.getElementById('breeds');
 
 //Blinking eyes 
-
 let blinking = gsap.timeline({repeatDelay:.3, repeat:-1, yoyo:true});
 blinking.to("#eyes", {delay:3, opacity:0, duration:.1}).to("#closedEyes", {opacity:1, duration:.2});
 
@@ -16,9 +16,11 @@ mouth.to("#mouth",{y:12, duration:.8})
 function quiet(){
     mouth.reverse(1)
 }
-btn.addEventListener('click', barking)
 
-function barking(){
-    mouth.play(0)
-    barkAudio.play()   
-}
+//Getting values
+getValue.addEventListener('change', function(){
+   breed = getValue.value    
+   mouth.play()
+   barkAudio.play()
+})
+
